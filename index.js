eaf95536da9454a7fa975ed756fd30fc35c42159
Christoph Knittel <christoph.knittel@cca.io>
2020-06-19 16:20:54 +0200

    0.9.0

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var VERSION = require("./package.json").version;
-
 var path = require("path");
 
 module.exports =
   process.platform === "darwin"
     ? path.join(__dirname, "macos", "bs-react-intl-extractor")
     : process.platform === "linux" && process.arch === "x64"
-      ? path.join(__dirname, "linux", "bs-react-intl-extractor")
-      : null;
+    ? path.join(__dirname, "linux", "bs-react-intl-extractor")
+    : process.platform === "win32" && process.arch === "x64"
+    ? path.join(__dirname, "windows", "bs-react-intl-extractor.exe")
+    : null;
