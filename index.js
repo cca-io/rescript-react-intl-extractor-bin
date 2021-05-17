6e681f6d0f66200b0434cbcd54eb3a196e32c9dd
Christoph Knittel <ck@cca.io>
2021-05-17 19:33:04 +0200

    Deactivate Windows support for now due to Windows CI build issue

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ module.exports =
     ? path.join(__dirname, "macos", "rescript-react-intl-extractor")
     : process.platform === "linux" && process.arch === "x64"
     ? path.join(__dirname, "linux", "rescript-react-intl-extractor")
-    : process.platform === "win32" && process.arch === "x64"
-    ? path.join(__dirname, "windows", "rescript-react-intl-extractor.exe")
-    : null;
+    : // : process.platform === "win32" && process.arch === "x64"
+      // ? path.join(__dirname, "windows", "rescript-react-intl-extractor.exe")
+      null;
