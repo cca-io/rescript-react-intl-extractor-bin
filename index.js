9c5b69274a2821a9e44780bb9dffd302376a2e2b
Christoph Knittel <ck@cca.io>
2021-05-17 19:32:01 +0200

    Fix exe name

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ var path = require("path");
 
 module.exports =
   process.platform === "darwin"
-    ? path.join(__dirname, "macos", "bs-react-intl-extractor")
+    ? path.join(__dirname, "macos", "rescript-react-intl-extractor")
     : process.platform === "linux" && process.arch === "x64"
-    ? path.join(__dirname, "linux", "bs-react-intl-extractor")
+    ? path.join(__dirname, "linux", "rescript-react-intl-extractor")
     : process.platform === "win32" && process.arch === "x64"
-    ? path.join(__dirname, "windows", "bs-react-intl-extractor.exe")
+    ? path.join(__dirname, "windows", "rescript-react-intl-extractor.exe")
     : null;
